e2530761faee2cc580b516c4abfe4ec2dfd702b4
Alestln <itworkplace46@gmail.com>
2025-08-01 14:39:33 +0300

    added patroling

--- Scripts/Enemies/Opossum/opossum.gd
+++ Scripts/Enemies/Opossum/opossum.gd
@@ -1,41 +1,58 @@
 extends CharacterBody2D
 
 @onready var floor_detector: RayCast2D = $FloorDetector
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
 @onready var collision: CollisionShape2D = $CollisionShape
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
 @onready var player_detector_ray: RayCast2D = $PlayerDetectorRay
 
-@export var speed: float = 40.0
+@export var speed: float = 40
+@export var vision_distance: float = 120
 
-# Направление движения: 1 = вправо, -1 = влево
+enum State {
+	Patrol,
+	Chase
+}
+
+var currentState: State = State.Patrol
 var direction: int = -1
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func can_see_player() -> bool:
-	if not GameManager.player:
-		return false
-	
-	player_detector_ray.target_position = GameManager.player.global_position
-	player_detector_ray.enabled = true
-	
-	if player_detector_ray.is_colliding():
-		print("Colliding with player")
-	
-	return true
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	can_see_player()
+	match currentState:
+		State.Patrol:
+			update_patrol()
+		State.Chase:
+			pass
 	
+	move_and_slide()
+
+func update_patrol() -> void:
 	if not floor_detector.is_colliding():
 		change_direction()
 		update_visual_direction()
 	
 	velocity.x = direction * speed
+
+func can_see_player() -> bool:
+	if not GameManager.player:
+		return false
 	
-	#move_and_slide()
+	var to_player = to_local(GameManager.player.global_position)
+	if to_player.length() > vision_distance:
+		return false
+	
+	player_detector_ray.target_position = to_player
+	player_detector_ray.enabled = true
+	player_detector_ray.force_raycast_update()
+	
+	if player_detector_ray.is_colliding():
+		var collider = player_detector_ray.get_collider()
+		return collider == GameManager.player
+	
+	return false
 
 func change_direction():
 	direction *= -1
